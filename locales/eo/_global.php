539eb6f0e1643a8a7dae00c858f5b1e3185c676c
Keith Bowes <zooplah@gmail.com>
2015-05-09 23:31:27 -0400

    Allow country-agnostic language codes

--- locales/eo/_global.php
+++ locales/eo/_global.php
@@ -5,7 +5,7 @@
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
-$trans['eo_EO'] = array(
+$trans['eo'] = array(
 '__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
 '' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2015-04-06 01:31-0400\nPO-Revision-Date: 2015-04-06 01:31-0400\nLast-Translator: Keith <zooplah@gmail.com>\nLanguage-Team: Esperanto\nLanguage: eo\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
 "Self-promotion: To read RSS 3.0 feeds, you can download my feed\n# reader:\n" => "Reklamo: Mia fluo-legilo uzeblas por legi RSS3-fluojn:\n",
