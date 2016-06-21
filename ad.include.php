ef31a87ed0d57c151923d7427cb613b806eefc0d
Keith Bowes <keithbowes@users.noreply.github.com>
2016-06-20 22:46:08 -0400

    Updated the links to to my feed reader

--- ad.include.php
+++ ad.include.php
@@ -1,6 +1,5 @@
 
 #
 # <?php echo $Skin->T_("Self-promotion: To read RSS 3.0 feeds, you can download my feed\n# reader:\n"); ?>
-# http://sourceforge.net/projects/r3r
+# http://github.com/keithbowes/r3r
 #
-
